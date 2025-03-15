83f59e31be7b20764418d7b68e6baf2a7961f9d6
Temochka <146855997+tteemma@users.noreply.github.com>
2025-03-15 19:35:13 +0300

    Delete cryptowallet directory

--- cryptowallet/src/utils.js
+++
@@ -1,7 +0,0 @@
-
-export function percentDiffrents(asset, coin) {
-    return (100 * Math.abs((asset - coin) / ((asset + coin) / 2))).toFixed(2)
-}
-export function capitalize(str){
-    return str.charAt(0).toUpperCase() + str.substr(1)
-}
